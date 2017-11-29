8ea3c2a931cfd30da578757917a0c0e0ed6c6b69
Peter Hudec <phudec@cnc.sk>
2017-11-29 12:51:25 +0100

    added to_python filter

--- filter_plugins/config_convert.py
+++ filter_plugins/config_convert.py
@@ -8,16 +8,6 @@ import json
 
 from ansible import errors
 
-class ConfigRubyEncoder(json.JSONEncoder):
-    def default(self, obj):
-        if (obj is None):
-            return 'nil'
-        if (obj is True):
-            return 'true'
-        if (obj is False):
-            return 'false'
-        return super(ConfigRubyEncoder, self).default(self, obj)
-
 def to_ruby(a,):
     if bool(a is None):
         return 'nil'
@@ -40,11 +30,33 @@ def to_ruby(a,):
         for k,v in a.iteritems():
             items.append("%s: %s" % (to_ruby(k), to_ruby(v)))
         return "{%s}" % ','.join(items)
-    return ''
-#    return json.dumps(a, cls=ConfigRubyEncoder, *args, **kw)
+
+    raise ValueError('unsupported type %s' % type(a))
 
 def to_python(a):
-    return None
+    if bool(a is None):
+        return str(a)
+    if bool(a is True):
+        return str(a)
+    if bool(a is False):
+        return str(a)
+    if bool(isinstance(a, basestring)):
+        return "'%s'" % a
+    if bool(isinstance(a, (int, float))):
+        return str(a)
+    if bool(isinstance(a, (list, tuple))):
+        items = []
+        for v in a:
+            items.append(to_python(v))
+        return "[%s]" % ','.join(items)
+
+    if bool(isinstance(a, dict)):
+        items = []
+        for k,v in a.iteritems():
+            items.append("%s: %s" % (to_python(k), to_python(v)))
+        return "{%s}" % ','.join(items)
+
+    raise ValueError('unsupported type %s' % type(a))
 
 
 class FilterModule(object):
